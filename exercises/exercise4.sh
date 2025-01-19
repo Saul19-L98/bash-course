f189b3f470b4ff0c6b55e45834ec48da44188623
sam <saul.alejandro19@gmail.com>
2025-01-18 18:00:18 -0600

    Exercise 4 added.

--- exercises/exercise4.sh
+++ exercises/exercise4.sh
@@ -2,7 +2,6 @@
 #!/bin/bash
 
 id=0
-content=""
 option=""
 options=(
   "Current Processes"
@@ -16,9 +15,13 @@ options=(
   "Exit"
 )
 
-copy=${options[*]}
-copyLength=${#copy[@]}
 optionsLength=${#options[@]}
+copy=()
+for option in "${options[@]}"
+do
+	copy+=("$option")
+done
+copyLength=${#copy[@]}
 
 while :
 do
@@ -27,72 +30,72 @@ do
   echo "-------Options-------"
   for ((i=0; i < $optionsLength; i++))
   do
-    echo "$i. ${options[i]}"
+	  echo "$((i + 1)). ${options[i]}"
   done
 
   read -n1 -p "Select an option: " option
 
   case $option in
     1)
-      echo "Current Process is a bash program."
+      echo -e "\nCurrent Process is a bash program."
       id=0
       content=${options[0]}
       sleep 3
       ;;
     2)
-      echo "60% of memory is available."
+      echo -e "\n60% of memory is available."
       id=1
       content=${options[1]}
       sleep 3
       ;;
     3)
-      echo "30% of the disk remains available."
+      echo -e "\n30% of the disk remains available."
       id=2
       content=${options[2]}
       sleep 3
       ;;
     4)
-      echo "Network data is restricted."
+      echo -e "\nNetwork data is restricted."
       id=3
       content=${options[3]}
       sleep 3
       ;;
     5)
-      echo "Config Data is set for js."
+      echo -e "\nConfig Data is set for js."
       id=4
       content=${options[4]}
       sleep 3
       ;;
     6)
-      echo "Program data is set to bash."
+      echo -e "\nProgram data is set to bash."
       id=5
       content=${options[5]}
       sleep 3
       ;;
     7)
-      echo "Backup created."
+      echo -e "\nBackup created."
       id=6
       content=${options[6]}
       sleep 3
       ;;
     8)
-      echo "Create an option."
-      id=7
-      content=${options[7]}
+      echo -e "\nCreate an option."
+      read -p "Add a new option: " newOption
+      options+=("$newOption")
       sleep 3
       ;;
     9)
-      echo "Exiting operations"
-      exit 0
+      echo -e "\nExiting operations"
+      break
       sleep 3
       ;;
   esac
 done
 
-if[[ $optionsLength == $copyLength ]]; then
-  echo "The options array has been modified."
-  echo "The new array is: ${options[$#options[@]]}"
-else
+if [[ ${#options[*]} = $copyLength ]] then
   echo "The options array has not been modified."
-    echo "The original array is: ${copy[$id]}"
+  echo "The original array is: ${copy[id]}"
+else
+  echo "The options array has been modified."
+  echo "The new array is: ${options[-1]}"
 fi
