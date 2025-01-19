a199f4462fcfe7ac6ecce3522bac84c2c414ee04
sam <saul.alejandro19@gmail.com>
2025-01-19 16:17:11 -0600

    new script added.

---
+++ 23_transfer_packages.sh
@@ -0,0 +1,14 @@
+# ! /bin/bash
+
+echo "Package all the scripts in a directory named shellcoruse and transfer them throw the network to another device using rsync command."
+
+host=""
+user=""
+
+read -p "Host: " host
+read -p "User: " user
+echo -e "\nPackaging process has started."
+
+rsync -avz $(pwd) $user@$host:/c/Users/salla/Downloads
+
+echo -e "\nTransfer completed"
