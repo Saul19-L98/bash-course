651035179c11063d2d7cf8682de718acaa9892be
sam <saul.alejandro19@gmail.com>
2025-01-21 10:39:14 -0600

    functions to install and manage postgresql backups is integrated.

--- 25_functions_adding_postgress.sh
+++ 25_functions_adding_postgress.sh
@@ -1,6 +1,7 @@
 # ! /bin/bash
 
 option=0
+currentDate=`date +%Y%m%d`
 
 install_postgres() {
 	echo -e "\nInstalling Postgres..."
@@ -37,13 +38,49 @@ uninstall_postgres() {
 }
 
 create_backup(){	
-	echo -e "\nCreate Postgress backup"
-	sleep 3
+	echo "List all dbs"
+	sudo -u postgres psql -c "\l"
+	read -p "Choose the db to backup: " dbBackup
+	read -p "Sudo password: " password
+	read -p "Backup dir: " backupPath
+	echo "dir: $backupPath"
+	echo -e "\n"
+	if [ -d "$backupPath" ]; then
+		echo "Stablish directory permissions."
+		echo "$password" | sudo -S chmod 755 ${backupPath}
+		echo "Backup initiated..."
+		sudo -u postgres pg_dump -Fc $dbBackup > "$backupPath/bdBackup${currentDate}.bak"
+		echo "Backup created successfuly in the path: $backupPath/dbBackup${currentDate}.bak"
+	else 
+		echo "Directory does not exists."
+	fi	
+	read -n 1 -s -r -p "Press {ENTER} to continue..."
 }
 
-restart_backup(){
-	echo -e "\nRestarting Postgress backup"
-	sleep 3
+restore_backup(){
+	echo -e "List Backups"
+	read -p "Enter the directory where the backups are: " directory
+	ls -la $directory
+	read -p "Choose backup to restore: " backupToRestore
+       	echo -e "\n"
+	read -p "Choose the destination db name: " destination
+    	#Verify is db exists
+	verifyDb=$(sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -wq $destination)
+	if [ $? -eq 0  ]; then
+		echo "Restoring in destination db: $destination"
+	else
+		sudo -u postgresql psql -c "create database $destination"
+	fi
+
+	if [ -f "$backupToRestore" ]; then
+		echo "Resotring backup..."
+		sudo -u postgres pg_restore -Fc -d $destination "$directory/$backupToRestore"
+		echo "List all dbs"
+		sudo -u postgres psql -c "\l"
+	else
+		echo "The backup $backupToRestore does not exists."
+	fi
+	read -n 1 -s -r -p "Press {ENTER} to continue..."
 }
 
 exit_program(){	
@@ -55,27 +92,27 @@ exit_program(){
 while :
 do	
 clear
-echo"Initial Menu"
+echo "Initial Menu"
 echo "1. Postgress install"
 echo "2. Uninstall postgress"
 echo "3. Create backup"
-echo "4. Backup Restart"
+echo "4. Restore backup"
 echo "5. Exit"
 
 read -n1 -p "Enter options [1-5]:" option
 case $option in
 	1) 
-		install_postgress
+		install_postgres
 		;;
 	2)
-	       	uninstall_postgress	
+	       	uninstall_postgres	
 		;;
 	3) 
 		create_backup
 		;;
 
 	4) 
-		restart_backup
+		restore_backup
 		;;
 
 	5)
